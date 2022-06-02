762b3ee8fd6fca20e76b3d4cc0a9e81ab84cfb03
daddysorengod <vuhungtien2000@gmail.com>
2022-06-02 09:37:29 +0700

    fix new lap

--- env/DatabaseConfig.py
+++ env/DatabaseConfig.py
@@ -2,4 +2,3 @@ import motor.motor_asyncio
 
 myClient = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://daddysorengod:vht01042000@cluster0.ejgjx.mongodb.net/Booking?retryWrites=true&w=majority")
 db = myClient['Booking']
-
--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\IT\Project\booking_api\env
+set VIRTUAL_ENV=D:\IT\project\booking_api\env
 
 if not defined PROMPT set PROMPT=$P$G
 
